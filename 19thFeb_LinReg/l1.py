a875458fc92c01c9a5251f25897e6f80382cfbab
Pateljaivi <jaivipatel2005@gmail.com>
2026-02-23 11:59:57 +0530

    ADD : add comment in l1.py

--- 19thFeb_LinReg/l1.py
+++ 19thFeb_LinReg/l1.py
@@ -5,29 +5,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from sklearn.compose import ColumnTransformer
-from sklearn.model_selection import train_test_split
+#OneHotEncoder -> Categorical data ko numbers me convert karta hai
+#standardscaler -> data ko scale karta hai
+from sklearn.model_selection import train_test_split #dataset ko train test me split karta hai
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 
 
 df = pd.read_csv("insurance.csv")
-df.head()
+df.head() #first 5 rows
 
-df.info()
+df.info() #check columns's datatype and null values
 
-df.describe()
+df.describe() #give statistical summary of numerical columns(mean,std,min,max)
 
-df.isnull().sum()
+df.isnull().sum() #check missing values in each column
+
+print("Duplicate rows:",df.duplicated().sum()) #print how many duplicate row exist
+
+df = df.drop_duplicates() #remove duplicate row
 
 print("Duplicate rows:",df.duplicated().sum())
 
-df = df.drop_duplicates()
+print(df.shape) #give count of rows and columns
 
-print("Duplicate rows:",df.duplicated().sum())
+#Data visualization
 
-print(df.shape)
-
+#distribution of charges->histogram,kde(kernel density estimation)->smooth curve jo overall patten show karta hai
 plt.figure()
 sns.histplot(df["charges"], kde=True)
 plt.title("Distribution of Charges")
@@ -35,18 +39,25 @@ plt.show()
 
 plt.figure()
 sns.heatmap(df.corr(numeric_only=True), annot=True,cmap="coolwarm")
+#df.corr() -> dataframe ke columns ke beech correlation calculate karta hai,numeric_only=True-> apply correlation on only numeric columns
+#annot = True -> show correlation value inside all boxes
 plt.show()
 
+#boxplot of smoker vs charges
 plt.figure()
 sns.boxplot(x="smoker", y="charges", data=df)
 plt.show()
 
-plt.figure(figsize=(8,5))
-sns.boxplot(x=df["charges"])
-plt.title("Boxplot of Charges")
-plt.show()
+# plt.figure(figsize=(8,5))
+# sns.boxplot(x=df["charges"])
+# plt.title("Boxplot of Charges")
+# plt.show()
 
 print(df.shape)
+
+
+#Log Transformation
+
 
 df["charges"] = np.log(df["charges"])
 
@@ -54,32 +65,46 @@ sns.histplot(df["charges"], kde=True)
 plt.title("Log Transformed Charges")
 plt.show()
 
+#convert categorical to numerical
 df = pd.get_dummies(df,drop_first=True)
+#suppose we have 2 columns in category so dorp_first -> remove one
 
 print(df.columns)
 
 print(df.shape)
 
+
+#create new feature
 df["bmi_smoker"] = df["bmi"] * df["smoker_yes"]
 #smoker + high BMI ->extremely high charges
 #Non - smoker + High BMI -> moderate increase
 #this not linear relation
 
+#Define X and Y
 X = df.drop("charges", axis=1)
 y = df["charges"]
 
+
+#Feature Scaling -> all value lie between -1 to +1
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
+
+#train test split
 X_train, X_test, y_train, y_test = train_test_split(
     X_scaled, y, test_size=0.2, random_state=42
 )
+#random_state = 42 same result when run
 
+
+#Model training
 model = LinearRegression()
 model.fit(X_train, y_train)
 
+#predict on test data
 y_pred = model.predict(X_test)
 
+#r2 score-> tell how nicely fit model in range from 0 to 1
 r2 = r2_score(y_test, y_pred)
 mse = mean_squared_error(y_test, y_pred)
 rmse = np.sqrt(mse)
