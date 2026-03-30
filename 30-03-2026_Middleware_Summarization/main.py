1a88f89b496b488aa649244ad47ca5f7413606b2
Pateljaivi <jaivipatel2005@gmail.com>
2026-03-30 17:33:39 +0530

    ADD : create agent for chatbot

---
+++ 30-03-2026_Middleware_Summarization/main.py
@@ -0,0 +1,14 @@
+from agent import AI_Agent
+
+if __name__ == "__main__":
+    bot = AI_Agent()
+    user_id = "chat_user_1"
+
+    print("--- Simple Chat Started (No DB) ---")
+    while True:
+        text = input("You: ")
+        if text.lower() == "exit":
+            break
+
+        response = bot.run(user_id, text)
+        print(f"AI: {response}")
